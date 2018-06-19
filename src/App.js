57e7fe37bf349bb303c4a61bce3e48c69f656116
Piotr Misiuk <pmisiuk@gmail.com>
2018-06-19 14:28:03 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -94,6 +94,124 @@ class App extends Component {
             </div>
           </div>
         </main>
+        <footer className="footer">
+    <div className="container">
+      <div className="columns is-multiline is-mobile">
+        <div className="column is-2-tablet is-6-mobile is-first">
+          <h5 className="footer-title"><app-link to="Home">Userfeeds</app-link></h5>
+          <ul className="footer-links">
+            <li>
+              <app-link to="Protocol">Protocol</app-link>
+            </li>
+            <li>
+              <app-link to="About">About</app-link>
+            </li>
+            <li>
+              <app-link to="Careers">Careers</app-link>
+            </li>
+          </ul>
+        </div>
+        <div className="column is-2-tablet is-6-mobile">
+          <h5 className="footer-title">Use cases</h5>
+          <ul className="footer-links">
+            <li>
+              <app-link to="Collectibles">Collectibles</app-link>
+            </li>
+            <li>
+              <app-link to="Airdrop">Airdrop</app-link>
+            </li>
+            <li>
+              <app-link to="Annotations">Annotations</app-link>
+            </li>
+            <li>
+              <app-link to="Newsfeeds">Newsfeeds</app-link>
+            </li>
+            <li>
+              <app-link to="Messaging">Messaging</app-link>
+            </li>
+
+          </ul>
+        </div>
+        <div className="column is-2-tablet is-6-mobile">
+          <h5 className="footer-title">Developers</h5>
+          <ul className="footer-links">
+            <li>
+              <app-link to="https://userfeeds-platform.readthedocs-hosted.com/en/latest/">Documentation</app-link>
+            </li>
+            <li>
+              <app-link to="Examples">Examples</app-link>
+            </li>
+            <li>
+              <app-link to="http://stats.pingdom.com/fj4x6r76hd8z">API Status</app-link>
+            </li>
+          </ul>
+        </div>
+        <div className="column is-2-tablet is-6-mobile">
+          <h5 className="footer-title">Products</h5>
+          <ul className="footer-links">
+            <li>
+              <app-link to="http://linkexchange.io" className="is-target-blank">
+                Link Exchange <i className="rarr" aria-hidden="true"></i>
+              </app-link>
+            </li>
+            <li>
+              <app-link to="https://userfeeds.github.io/cryptopurr/">
+                Purr<i className="rarr" aria-hidden="true"></i>
+              </app-link>
+              <app-link to="https://userfeeds.github.io/cryptobeep/">
+                Beep<i className="rarr" aria-hidden="true"></i>
+              </app-link>
+              <app-link to="https://userfeeds.github.io/cryptomoji/">
+                Moji<i className="rarr" aria-hidden="true"></i>
+              </app-link>
+              <app-link to="https://userfeeds.github.io/robohash-book/">
+                Hash<i className="rarr" aria-hidden="true"></i>
+              </app-link>
+            </li>
+          </ul>
+        </div>
+        <div className="column is-4-tablet is-12-mobile is-links">
+          <div className="footer-social">
+            <a href="https://t.me/userfeeds">
+              <img src="assets/logo-footer-telegram.svg" width="50" height="50" alt="Telegram"/>
+              <strong>Telegram</strong>
+            </a>
+            <a href="https://gitter.im/Userfeeds/">
+              <img src="assets/logo-footer-gitter.svg" width="50" height="50" alt="Gitter"/>
+              <strong>Gitter</strong>
+            </a>
+            <a href="https://medium.com/userfeeds">
+              <img src="assets/logo-footer-medium.svg" width="50" height="50" alt="Medium"/>
+              <strong>Medium</strong>
+            </a>
+            <a href="https://twitter.com/userfeeds">
+              <img src="assets/logo-footer-twitter.svg" width="50" height="50" alt="Twitter"/>
+              <strong>Twitter</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-4 is-newsletter">
+          <h5 className="footer-title">Sign up for updates</h5>
+          <form action="https://userfeeds.us17.list-manage.com/subscribe/post?u=9cf96f96e2410ddcc7ccd1577&amp;id=0317d71772" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+            <div className="footer-form field has-addons">
+              <div className="control">
+                <input className="input is-large" placeholder="Your email address" value="" type="email" id="mce-EMAIL" name="EMAIL"/>
+              </div>
+              <div className="control">
+                <button className="button is-large is-info" type="submit"  value="Subscribe me" name="subscribe">
+                  Subscribe me
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </footer>
       </div>
     );
   }
