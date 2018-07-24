9d97b0a9e1817f5a138277bc3653e2a7a0db08aa
Kamil Mendelski <kamil@frontlabs.pl>
2018-07-24 12:09:17 +0200

    Header & Navbar components for equal examples design

--- examples/interfaces/src/App.js
+++ examples/interfaces/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
-import './App.scss';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+
+import './App.css';
 
 class App extends Component {
   constructor () {
@@ -23,33 +26,29 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Userfeeds platform Example Interface.</h1>
-        </header>
-        <p className="App-intro">
-          Values that where sent using `credits` key inside `claims` (json messages used by Userfeeds Platform to send information) from all claims ever sent.
+        <Navbar />
+        <main>
+          <Header title="Interfaces" subtitle="Values that where sent using `credits` key inside `claims` (json messages used by Userfeeds Platform to send information) from all claims ever sent. Basically it is popularity ranking based on how much usage each interface is generating." />
 
-          Basically it is popularity ranking based on how much usage each interface is generating.
-        </p>
-
-        <table>
-          <tbody>
-            <tr>
-              <th>Score</th>
-              <th>Interface</th>
-            </tr>
-            {this.state.items.map((item, index) =>
-              <tr key={`item-${index}`}>
-                <td>{item.sequence}</td>
-                <td><a href={item.credit_value}>{item.credit_value ? item.credit_value.substr(0, 100) : ""}</a></td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-
-        <p>
-          Source code for this app can be found at <a href="https://github.com/Userfeeds/examples/tree/master/examples/interfaces">https://github.com/Userfeeds/examples/tree/master/examples/interfaces</a>
-        </p>
+          <section className="interfaces">
+            <div className="container">
+              <table>
+                <tbody>
+                  <tr>
+                    <th>Score</th>
+                    <th>Interface</th>
+                  </tr>
+                  {this.state.items.map((item, index) =>
+                    <tr key={`item-${index}`}>
+                      <td>{item.sequence}</td>
+                      <td><a href={item.credit_value}>{item.credit_value ? item.credit_value.substr(0, 100) : ""}</a></td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </section>
+        </main>
       </div>
     );
   }
